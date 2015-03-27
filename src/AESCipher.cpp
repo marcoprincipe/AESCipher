ceb0c9dbbff7b60ce4408378343d216bd196e5ee
marcoprincipe <marco.principe@gmail.com>
2015-03-27 16:14:32 -0300

    Initial commit

---
+++ src/AESCipher.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : AESCipher.cpp
+// Author      : Marco Aurelio Principe Goncalves
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
